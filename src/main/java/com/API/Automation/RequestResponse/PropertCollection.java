71d42e0b4b15e84bc44f8916a31dc271fe1c7502
testpracticeap <testpracticeapi68@gmail.com>
2022-10-18 12:15:45 +0530

    Framework Initial Commit

---
+++ src/main/java/com/API/Automation/RequestResponse/PropertCollection.java
@@ -0,0 +1,16 @@
+package com.API.Automation.RequestResponse;
+
+import org.springframework.stereotype.Component;
+
+
+public  class PropertCollection {
+	
+	public   String URL;
+	public static  String classpath="file:///C:/Users/7000032427/eclipse-workspace/RestAssuredAutomation/";
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/API/Automation/Utility/PropertySourcesApp.java
@@ -0,0 +1,15 @@
+package com.API.Automation.Utility;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@PropertySource("file:///C:/Users/7000032427/eclipse-workspace/RestAssuredAutomation/src/main/resources/application.properties")
+public  class PropertySourcesApp {
+	
+	
+
+	
+
+}
